04780f05d3eb7e56443b5fb828a381c926eaf400
Diveyez <nemofbaltimore@gmail.com>
2019-04-02 12:03:28 -0700

    .

--- server-stats/include.php
+++ server-stats/include.php
@@ -1,1 +1,1 @@
-<?= $data["server_name"]; ?><?= $data["server_name"]; ?><?= $data["server_name"]; ?>
+<?= $data["server_name"]; ?><?= $data["game_mode"]; ?><?= $data["player_count"]; ?><?= $data["players"]; ?><?= $data["warp_mode"]; ?>
--- server-stats/index.php
+++ server-stats/index.php
@@ -1,1 +1,2 @@
 <?php $data = json_decode(file_get_contents("http://ksp.devgru.club:8080/%22),true); ?>
+<?= $data["server_name"]; ?><?= $data["game_mode"]; ?><?= $data["player_count"]; ?><?= $data["players"]; ?><?= $data["warp_mode"]; ?>
